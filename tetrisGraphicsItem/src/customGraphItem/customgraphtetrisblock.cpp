d8967b986aa884e823d89a250994d9b70042ef95
zhoutk <zhoutk@189.cn>
2021-04-27 12:21:38 +0800

    add customGraphTetrisBolck base.

--- tetrisGraphicsItem/src/customGraphItem/customgraphtetrisblock.cpp
+++ tetrisGraphicsItem/src/customGraphItem/customgraphtetrisblock.cpp
@@ -4,3 +4,23 @@ CustomGraphTetrisBlock::CustomGraphTetrisBlock()
 {
 
 }
+
+QRectF CustomGraphTetrisBlock::boundingRect() const
+{
+	throw std::logic_error("The method or operation is not implemented.");
+}
+
+int CustomGraphTetrisBlock::type() const
+{
+	return Type;
+}
+
+Qjson CustomGraphTetrisBlock::getFactors()
+{
+	return Qjson();
+}
+
+void CustomGraphTetrisBlock::paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget /*= nullptr*/)
+{
+	throw std::logic_error("The method or operation is not implemented.");
+}
--- tetrisGraphicsItem/src/customGraphItem/customgraphtetrisblock.h
+++ tetrisGraphicsItem/src/customGraphItem/customgraphtetrisblock.h
@@ -1,11 +1,26 @@
 #ifndef CUSTOMGRAPHTETRISBLOCK_H
 #define CUSTOMGRAPHTETRISBLOCK_H
 #include "customgraphbase.h"
+#include <QPainter>
+#include "customgraphitemtype.h"
+#include "../common/Qjson.h"
+#include "../common/utils.h"
 
 class CustomGraphTetrisBlock : public CustomGraphBase
 {
+	Q_OBJECT
+		enum {
+		Type = TETRISBLOCKTYPE
+	};
 public:
     CustomGraphTetrisBlock();
+
+	QRectF boundingRect() const override;
+	int type() const override;
+	Qjson getFactors() override;
+
+	void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget = nullptr) override;
+
 };
 
 #endif // CUSTOMGRAPHTETRISBLOCK_H
